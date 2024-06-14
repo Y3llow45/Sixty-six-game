73407b36782cfe075c538e4e27367da785425566
Borislav Hristov <bofengejn@gmail.com>
2024-06-14 13:51:02 +0300

    Update Game

--- sixty-six-game/src/components/Game/Card/Card.js
+++ sixty-six-game/src/components/Game/Card/Card.js
@@ -1,8 +1,14 @@
+import './Card.css'
+
 function Card({ key, image, card }) {
+
+  function selectCard() {
+    console.log(card);
+  }
+
   return (
-    <div>
-      {console.log(image)}
-      <img src={`/cards/${image}`} alt={card} key={key} />
+    <div className="card">
+      <img src={`/cards/${image}`} alt={card} key={key} onClick={selectCard.bind(card)} />
       <p>{card}</p>
     </div>
   );
--- sixty-six-game/src/components/Game/Game.js
+++ sixty-six-game/src/components/Game/Game.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { generateShuffledDeck } from '../../utils';
 import Card from './Card/Card';
+import './Game.css';
 
 export default class Game extends Component {
   constructor(props) {
@@ -10,8 +11,10 @@ export default class Game extends Component {
       opponent: [],
       center: [],
       player: [],
+      opponentHands: [],
+      playerHands: [],
       cardMapping: [
-        { image: '1.png', card: 'HA' }, { image: '2.png', card: 'H10' }, { image: '3.png', card: 'HK' }, { image: '4.png', card: 'HQ' }, { image: '5.png', card: 'HJ' }, { image: '6.png', card: 'H9' },
+        { image: '1.png', card: 'A', suit: 'h', points: 11 }, { image: '2.png', card: 'H10' }, { image: '3.png', card: 'HK' }, { image: '4.png', card: 'HQ' }, { image: '5.png', card: 'HJ' }, { image: '6.png', card: 'H9' },
         { image: '7.png', card: 'CA' }, { image: '8.png', card: 'C10' }, { image: '9.png', card: 'CK' }, { image: '10.png', card: 'CQ' }, { image: '11.png', card: 'CJ' }, { image: '12.png', card: 'C9' },
         { image: '13.png', card: 'DA' }, { image: '14.png', card: 'D10' }, { image: '15.png', card: 'DK' }, { image: '16.png', card: 'DQ' }, { image: '17.png', card: 'SJ' }, { image: '18.png', card: 'D9' },
         { image: '19.png', card: 'SA' }, { image: '20.png', card: 'S10' }, { image: '21.png', card: 'SK' }, { image: '22.png', card: 'SQ' }, { image: '23.png', card: 'SJ' }, { image: '24.png', card: 'S9' }
@@ -27,30 +30,32 @@ export default class Game extends Component {
       player: shuffledDeck.slice(6, 12),
       center: shuffledDeck[12]
     }, () => {
-      this.renderCards();
+      this.startGame();
     });
   };
 
-  renderCards = () => {
+  startGame = () => {
 
   }
 
   render() {
     return (
-      <div>
-        <button onClick={this.handleGenerateDeck}>START</button>
+      <div className='game'>
+        <button onClick={this.handleGenerateDeck} className='btnStart'>START</button>
         <div className='opponent'>
-          {this.state.player.map(cardIndex => {
-            const card = this.state.cardMapping[cardIndex - 1];
-            console.log(card.image, card.card);
-            return <Card key={cardIndex} image={card.image} card={card.card} />;
+          {this.state.opponent.map(a => {
+            return <Card key={a} image={'back.png'} card={'back'} />;
           })}
         </div>
         <div className='center'>
 
         </div>
         <div className='player'>
-
+          {this.state.player.map(cardIndex => {
+            const card = this.state.cardMapping[cardIndex - 1];
+            console.log(card.image, card.card);
+            return <Card key={cardIndex} image={card.image} card={card.card} />;
+          })}
         </div>
       </div>
     )
