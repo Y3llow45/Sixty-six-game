1a97ff753beff2732460a7207eeb579d7c348e63
Borislav Hristov <bofengejn@gmail.com>
2024-06-16 11:33:40 +0300

    Update Game

--- sixty-six-game/src/components/Game/Game.js
+++ sixty-six-game/src/components/Game/Game.js
@@ -8,28 +8,23 @@ export default class Game extends Component {
     super(props);
 
     this.state = {
-      opponent: [],
-      center: [],
-      player: [],
-      opponentHands: [],
-      playerHands: [],
+      deck: [],           // cards to draw
+      opponent: [],       // opponent's cards
+      player: [],         // player's cards
+      trumpSuit: '',
+      opponentHands: [],  // hands won by opponent
+      playerHands: [],    // hands won by player
+      opponentSelection: '',
+      playerSelection: '',
+      isPlayerFirst: true,
       cardMapping: [
-        { image: '1.png', card: 'A', suit: 'h', points: 11 }, { image: '2.png', card: '10', suit: 'h', points: 10 }, { image: '3.png', card: 'K', suit: 'h', points: 4 }, { image: '4.png', card: 'Q', suit: 'h', points: 3 }, { image: '5.png', card: 'J', suit: 'h', points: 2 }, { image: '6.png', card: '9', suit: 'h', points: 0 },
-        { image: '7.png', card: 'A', suit: 'c', points: 11 }, { image: '8.png', card: '10', suit: 'c', points: 10 }, { image: '9.png', card: 'K', suit: 'c', points: 4 }, { image: '10.png', card: 'Q', suit: 'c', points: 3 }, { image: '11.png', card: 'J', suit: 'c', points: 2 }, { image: '12.png', card: '9', suit: 'c', points: 0 },
-        { image: '13.png', card: 'A', suit: 'd', points: 11 }, { image: '14.png', card: '10', suit: 'd', points: 10 }, { image: '15.png', card: 'K', suit: 'd', points: 4 }, { image: '16.png', card: 'Q', suit: 'd', points: 3 }, { image: '17.png', card: 'J', suit: 'd', points: 2 }, { image: '18.png', card: '9', suit: 'd', points: 0 },
-        { image: '19.png', card: 'A', suit: 's', points: 11 }, { image: '20.png', card: '10', suit: 's', points: 10 }, { image: '21.png', card: 'K', suit: 's', points: 4 }, { image: '22.png', card: 'Q', suit: 's', points: 3 }, { image: '23.png', card: 'J', suit: 's', points: 2 }, { image: '24.png', card: '9', suit: 's', points: 0 }
+        { image: '0.png', card: 'A', suit: 'h', points: 11 }, { image: '1.png', card: '10', suit: 'h', points: 10 }, { image: '2.png', card: 'K', suit: 'h', points: 4 }, { image: '3.png', card: 'Q', suit: 'h', points: 3 }, { image: '4.png', card: 'J', suit: 'h', points: 2 }, { image: '5.png', card: '9', suit: 'h', points: 0 },
+        { image: '6.png', card: 'A', suit: 'c', points: 11 }, { image: '7.png', card: '10', suit: 'c', points: 10 }, { image: '8.png', card: 'K', suit: 'c', points: 4 }, { image: '9.png', card: 'Q', suit: 'c', points: 3 }, { image: '10.png', card: 'J', suit: 'c', points: 2 }, { image: '11.png', card: '9', suit: 'c', points: 0 },
+        { image: '12.png', card: 'A', suit: 'd', points: 11 }, { image: '13.png', card: '10', suit: 'd', points: 10 }, { image: '14.png', card: 'K', suit: 'd', points: 4 }, { image: '15.png', card: 'Q', suit: 'd', points: 3 }, { image: '16.png', card: 'J', suit: 'd', points: 2 }, { image: '17.png', card: '9', suit: 'd', points: 0 },
+        { image: '18.png', card: 'A', suit: 's', points: 11 }, { image: '19.png', card: '10', suit: 's', points: 10 }, { image: '20.png', card: 'K', suit: 's', points: 4 }, { image: '21.png', card: 'Q', suit: 's', points: 3 }, { image: '22.png', card: 'J', suit: 's', points: 2 }, { image: '23.png', card: '9', suit: 's', points: 0 }
       ]
     };
   }
-
-  handleCardClick = (card) => {
-    this.setState((prevState) => ({
-      center: [...prevState.center, card],
-      player: prevState.player.filter(c => c !== card)
-    }), () => {
-      this.opponentTurn();
-    });
-  };
 
   handleGenerateDeck = () => {
     const shuffledDeck = generateShuffledDeck();
@@ -37,25 +32,64 @@ export default class Game extends Component {
     this.setState({
       opponent: shuffledDeck.slice(0, 6),
       player: shuffledDeck.slice(6, 12),
-      center: shuffledDeck[12]
+      trumpSuit: this.state.cardMapping[shuffledDeck[12]].suit,
+      deck: shuffledDeck.slice(13)
     }, () => {
-      this.startGame();
+      //console.log(`OP: ${this.state.opponent}, PL: ${this.state.player}, CE: ${this.state.center}, DECK: ${this.state.deck}`)
+      //this.startGame();
+      console.log(this.state.trumpSuit);
+      if (!this.state.isPlayerFirst) {
+        this.opponentTurn();
+      }
     });
   };
 
-  opponentTurn = () => {
-    const opponentCardIndex = Math.floor(Math.random() * this.state.opponent.length);
-    const opponentCard = this.state.opponent[opponentCardIndex];
-    this.setState((prevState) => ({
-      center: [...prevState.center, opponentCard],
-      opponent: prevState.opponent.filter(c => c !== opponentCard)
+  handleCardClick = (cardIndex) => {                               /// PLAYER
+    this.setState(() => ({
+      playerSelection: cardIndex
     }), () => {
-      this.compareCards();
+      console.log(this.state.playerSelection);
+      if (this.state.isPlayerFirst) {
+        this.opponentTurn();
+      }
+      else {
+        this.compareCards();
+      }
     });
   };
 
-  startGame = () => {
+  opponentTurn = () => {                                                               //OPPONENT
+    const randomIndex = Math.floor(Math.random() * this.state.opponent.length);
+    this.setState(() => ({
+      opponentSelection: this.state.opponent[randomIndex]
+    }), () => {
+      console.log(this.state.opponentSelection);
+      if (this.state.isPlayerFirst) {
+        this.compareCards();
+      }
+      //else => waiting for player
+    });
+  };
 
+  compareCards = () => {
+    console.log(this.state.opponentSelection, this.state.playerSelection);
+    let opponent = this.state.cardMapping[this.state.opponentSelection];
+    let player = this.state.cardMapping[this.state.playerSelection];
+    console.log(opponent, player);
+    console.log(this.state);
+    if (player.suit == opponent.suit) {
+      if (player.points > opponent.points) {
+        console.log(`player wins`);
+      } else {
+        console.log(`opponent wins`);
+      }
+    } else {
+      if (this.state.isPlayerFirst) {
+        console.log(`player wins`);
+      } else {
+        console.log(`opponent wins`);
+      }
+    }
   }
 
   render() {
@@ -73,8 +107,8 @@ export default class Game extends Component {
         <div className='player'>
           {this.state.player.map(cardIndex => {
             const card = this.state.cardMapping[cardIndex - 1];
-            console.log(card.image, card.card);
-            return <Card key={cardIndex} image={card.image} card={card.card} onClick={() => this.handleCardClick(card.card)} />;
+            //console.log(card.image, card.card, cardIndex);
+            return <Card key={cardIndex - 1} image={card.image} card={card.card} onClick={() => this.handleCardClick(cardIndex - 1)} />;
           })}
         </div>
       </div>
