4a0570b0c58799ba5d905d73c0e4b0497e80cf34
Borislav Hristov <bofengejn@gmail.com>
2024-07-18 09:23:32 +0300

    Add notify and router

--- sixty-six-game/src/App.js
+++ sixty-six-game/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
 import Game from './components/Game/Game';
+import { Switch, Route } from 'react-router-dom'
+import Register from './components/Register/Register';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 function App() {
   return (
     <div className="App">
-      <Game />
+      <ToastContainer
+        className="add-toast-container"
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable={false}
+        pauseOnHover={false}
+        theme="light"
+      />
+      <Switch>
+        <Route path='/' exact component={Game}></Route>
+        <Route path='/register' component={Register}></Route>
+      </Switch>
     </div>
   );
 }
