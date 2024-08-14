318fa5aa060320b9b4b27de2999fd4239294c8bf
Borislav Hristov <bofengejn@gmail.com>
2024-08-14 18:07:08 +0300

    Create sendCards.js

---
+++ server/services/sendCards.js
@@ -0,0 +1,14 @@
+const io = require('../socket');
+
+function sendCards(game) {
+  const roomData = io.sockets.adapter.rooms.get(game.room);
+  if (roomData) {
+    const socketsInRoom = Array.from(roomData);
+    const playerSocket = socketsInRoom[0];
+    const opponentSocket = socketsInRoom[1];
+    io.to(playerSocket).emit('cards', { player: game.player, opponent: game.opponent.length });
+    io.to(opponentSocket).emit('cards', { player: game.opponent, opponent: game.player.length });
+  }
+}
+
+module.exports = sendCards;
